3125bbe580097c3e985bf6f1d316f8f37cffd118
Yue Zhao <zhao13@ualberta.ca>
2024-08-05 18:21:40 +0100

    Added an argument
    bounds argument controls plotting of the boundaries for the ECDF plot.

--- utils/ecdf.py
+++ utils/ecdf.py
@@ -14,7 +14,17 @@ def get_ecdf(arr: np.ndarray, normalised: bool = True) -> Tuple[np.ndarray, np.n
     return x_new, y
 
 
-def plot_ecdf(arr: np.ndarray, ax: plt.Axes, normalised: bool = True, **kwargs) -> None:
+def plot_ecdf(arr: np.ndarray, ax: plt.Axes, bounds: Tuple[np.ndarray, np.ndarray] = None, normalised: bool = True, **kwargs) -> None:
     x, y = get_ecdf(arr, normalised=normalised)
+    if bounds:
+        arr_lo, arr_up = bounds
+        x_lo, y_lo = get_ecdf(arr_lo, normalised=normalised)
+        x_up, y_up = get_ecdf(arr_up, normalised=normalised)
 
-    ax.step(x, y, where="post", **kwargs)
+        ax.step(x, y, where="post", lw=1.5, **kwargs)
+        ax.step(x_lo, y_lo, where="post", lw=1.0, ls=":")
+        ax.step(x_up, y_up, where="post", lw=1.0, ls=":")
+        # ax.fill_betweenx(y, x_lo, x_up, step="post", alpha=0.5)
+
+    else:
+        ax.step(x, y, where="post", **kwargs)
