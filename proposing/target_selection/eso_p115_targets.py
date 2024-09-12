9e08399204a22a44699585bbc610baf998b1f391
Yue Zhao <zhao13@ualberta.ca>
2024-09-12 15:27:56 +0100

    Out path changed
    Catalogues saved under "high-v_sources" / "vlt_p115"

--- proposing/target_selection/eso_p115_targets.py
+++ proposing/target_selection/eso_p115_targets.py
@@ -7,9 +7,23 @@ def select_targets(verbose: bool = False) -> None:
     logger = VerboseLogger(verbose=verbose)
     logger.begin()
 
+    logger.log(f"Loading the catalogue ...")
     in_file = config.RESULTS_CATALOGUE_DIR / "high-v_sources" / "combined_vpec_gt_200.csv"
     df = pd.read_csv(in_file)
+    logger.log(f"Catalogue loaded; the input catalogue contains {df.shape[0]} entries.")
+
+    _filter_gmag = df["phot_g_mean_mag"] <= 16
+    _filter_fxfg = (df["fx_fg"] - df["fx_fg_err"] >= 10 ** (df["bp_rp"] - 3.5))
+    _filter_combined = _filter_gmag & _filter_fxfg
+
+    df_filtered = df[_filter_combined]
+    out_file = in_file.parent / "vlt_p115" / "vlt_p115_targets.csv"
+    df_filtered.to_csv(out_file, index=False)
+
+    logger.log(f"{df_filtered.shape[0]} sources selected.")
+    logger.log(f"Target catalogue saved to {out_file}.\n")
+    logger.end()
 
 
 if __name__ == "__main__":
-    select_targets()
+    select_targets(verbose=True)
