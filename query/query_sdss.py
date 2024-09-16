579e4d6cbcd827833f1c4812d6ec31eb5a78d72b
Yue Zhao <zhao13@ualberta.ca>
2024-09-16 11:14:03 +0100

    Script created

---
+++ query/query_sdss.py
@@ -0,0 +1,38 @@
+import config
+import pandas as pd
+import astropy.units as u
+from astroquery.sdss import SDSS
+from log.loggers import VerboseLogger
+from astropy.coordinates import SkyCoord
+
+
+def query_sdss(df: pd.DataFrame, verbose: bool = False, download_spectra: bool = False) -> None:
+    logger = VerboseLogger(verbose=verbose)
+    logger.begin()
+    logger.log(f"Loading input catalogue ...")
+    logger.log(f"{df.shape[0]} sources loaded.\n")
+    ra = df["ra"].values
+    dec = df["dec"].values
+
+    coords = SkyCoord(ra, dec, frame="icrs", unit="deg")
+    logger.log(f"Querying the SDSS archive for sources with spectroscopy matches ...")
+    results = SDSS.query_region(coords, radius=5. * u.arcsec, spectro=True)
+    logger.log("Done")
+    logger.log(f"{len(results)} matches found.\n")
+
+    if download_spectra:
+        logger.log(f"Retrieving spectra for {len(results)} matches ...")
+        hdul = SDSS.get_spectra(matches=results)
+
+    logger.end()
+
+
+def main() -> None:
+    df_targets = pd.read_csv(config.RESULTS_CATALOGUE_DIR / "high-v_sources"
+                             / "vlt_p115" / "vlt_p115_targets_w_observability.csv")
+
+    query_sdss(df_targets, verbose=True)
+
+
+if __name__ == "__main__":
+    main()
