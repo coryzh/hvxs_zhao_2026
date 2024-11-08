9ef8e4ae62cd12e3130ac0e8eb8de19efe8df4cb
Yue Zhao <zhao13@ualberta.ca>
2024-11-08 12:25:52 +0000

    Function added
    Function added to select a prime sample to be displayed in the paper.

--- catalog/select/selection_routines.py
+++ catalog/select/selection_routines.py
@@ -25,10 +25,28 @@ def select_high_fx_fg_ratio_sources(in_file_csv: Path, out_file: bool = False, v
     logger.end()
 
 
+def select_prime_sample(in_file: Path, verbose: bool = False) -> None:
+    logger = VerboseLogger(verbose=verbose)
+    df = pd.read_csv(in_file)
+
+    _vpec_filter = df["vpec_min_med"] >= 1000
+    _sep_filter = df["sep_x_g"] / df["pos_x_err"] <= 1.0
+
+    df_prime = df[_vpec_filter & _sep_filter]
+    df_prime = df_prime.sort_values(by=["from", "ra"], ascending=True)
+    logger.log(f"{df_prime.shape[0]} prime sources selected.")
+
+    out_file = in_file.parent / f"{in_file.stem}_prime.csv"
+    df_prime.to_csv(out_file, index=False)
+    logger.log(f"Selected sources saved to {out_file}.\n")
+    logger.end()
+
+
 def main() -> None:
     in_file = (config.RESULTS_CATALOGUE_DIR / "high-v_sources"
-               / "combined_vpec_lolim_gt_150_unique_stage_1.csv")
-    select_high_fx_fg_ratio_sources(in_file_csv=in_file, out_file=True, verbose=True)
+               / "combined_vpec_lolim_gt_150_unique_stage_6.csv")
+    # select_high_fx_fg_ratio_sources(in_file_csv=in_file, out_file=True, verbose=True)
+    select_prime_sample(in_file, verbose=True)
 
 
 if __name__ == "__main__":
