ece0cbaf25f97b6c2d6c56cccc70ea9de3c11b82
Yue Zhao <zhao13@ualberta.ca>
2025-01-24 18:02:51 +0000

    Function added
    A function added for adding the control sample histogram to the figure.

--- plot/plot_dist_histogram.py
+++ plot/plot_dist_histogram.py
@@ -7,6 +7,15 @@ from typing import Tuple
 from plot.plot_settings import DIST_INFERENCE_COLOR_DICT
 
 
+bins = np.linspace(0, 15, 80)
+bin_centers = 0.5 * (bins[1:] + bins[:-1])
+
+label_names = {
+    "simple_inversion": "Direct inversion",
+    "exp_model": "Bayesian method"
+}
+
+
 def set_up_figure() -> Tuple[plt.Figure, plt.Axes]:
     plt.style.use("mycustomised")
     fig, ax = plt.subplots(1, 1, figsize=(10, 8))
@@ -15,15 +24,14 @@ def set_up_figure() -> Tuple[plt.Figure, plt.Axes]:
 
     ax.set_yscale("log")
 
-    ax.set_yticks([1, 10, 100])
-    ax.set_yticklabels(["1", "10", "100"])
+    ax.set_yticks([1, 10, 100, 1000, 1e4])
+    ax.set_yticklabels(["1", "10", "100", "1000", r"$10^4$"])
 
     return fig, ax
 
 
 def add_hvxs(df: pd.DataFrame, ax: plt.Axes, fig: plt.Figure) -> None:
-    bins = np.linspace(0, 15, 80)
-    bin_centers = 0.5 * (bins[1:] + bins[:-1])
+    df = df[df["distance_inference"] != 1]
     # _ = ax.hist(df["dist_med"], bins=bins, ec="k", lw=2., histtype="step")
     for key, val in DIST_INFERENCE_COLOR_DICT.items():
         df_sub = df[df["distance_inference"] == key]
@@ -31,16 +39,25 @@ def add_hvxs(df: pd.DataFrame, ax: plt.Axes, fig: plt.Figure) -> None:
 
         hist = ax.hist(dist, bins=bins, histtype="step", ec=val, lw=1.5)
 
-        ax.step(bin_centers, hist[0], color=val, where="mid", label=key)
+        ax.step(bin_centers, hist[0], color=val, where="mid", label=label_names[key])
 
-        plt.legend(loc="best")
+
+def add_control(df: pd.DataFrame, ax: plt.Axes, fig: plt.Figure) -> None:
+    df = df[df["distance_inference"] != "fixed_at_1"]
+    dist = df["dist_med"]
+
+    hist = ax.hist(dist, bins=bins, histtype="step", ec="k", lw=1.5)
+
+    ax.step(bin_centers, hist[0], color="k", where="mid", label="Control")
 
 
 def make_figure() -> None:
     df = pd.read_csv(config.RESULTS_CATALOGUE_DIR / "high-v_sources" / "combined_vpec_lolim_gt_150_unique_stage_9.csv")
+    df_control = pd.read_csv(config.RESULTS_CATALOGUE_DIR / "control_sample" / "control_sample_stage_9.csv")
     fig, ax = set_up_figure()
     add_hvxs(df, ax, fig)
-
+    add_control(df_control, ax, fig)
+    plt.legend(loc="best")
     plt.savefig(config.RESULTS_FIGURES_DIR / "dist_histogram" / "test.pdf")
 
 
