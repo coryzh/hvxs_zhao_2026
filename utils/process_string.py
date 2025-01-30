b17934b279a74c32a91264f20bb397d3ef693b02
Yue Zhao <zhao13@ualberta.ca>
2025-01-30 15:59:31 +0000

    Added
    Adapted from the vpec_project repo. Note that the regular expression searches for any occurrences of "-" and "+" in a string, not necessary just leading occurrences.

---
+++ utils/process_string.py
@@ -0,0 +1,25 @@
+import re
+
+
+def process_name(name: str) -> str:
+    # Use a regular expression to find dashes between numbers
+    pattern = r'(\d)-(\d)'
+    replace = r'\1$-$\2'
+    new_name = re.sub(pattern, replace, name)
+
+    return new_name
+
+
+def wrap_sign(dec_str: str) -> str:
+    pattern = r'([-+])'
+    repl = r'$\1$'
+    return re.sub(pattern, repl, dec_str)
+
+
+def main() -> None:
+    test = "-53:10:01.1"
+    print(wrap_sign(test))
+
+
+if __name__ == "__main__":
+    main()
