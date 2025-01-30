5b396333bd7880c444cba02df69d2acf990b4f7b
Yue Zhao <zhao13@ualberta.ca>
2025-01-30 17:49:24 +0000

    Minor update
    X-ray ID strings reprocessed by wrapping $$ around the -/+ sign.

--- plot/plot_gaia_cmd.py
+++ plot/plot_gaia_cmd.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Tuple
 from matplotlib import colors
 from plot.plot_settings import SCATTER_DICT_CMD
+from utils.process_string import wrap_sign
 import plot.plot_settings as ps
 import config
 
@@ -46,8 +47,9 @@ def add_sample(in_file: Path, ax: plt.Axes) -> None:
     for i, row in df_prime.iterrows():
         bp_rp_prime = row["bp_rp"]
         g_abs_prime = row["phot_g_mean_mag"] - 5.0 * np.log10(row["dist_med"]) - 10.0
+        name = wrap_sign(row["ID_x"])
         ax.scatter(bp_rp_prime, g_abs_prime, fc=ps.PRIME_SOURCE_COLOR[i], marker=ps.PRIME_SOURCE_MARKER[i],
-                   label=row["ID_x"],
+                   label=name,
                    **ps.PRIME_SCATTER_MARKER_SETTINGS)
         # print(bp_rp_prime, row["dist_med"])
 
