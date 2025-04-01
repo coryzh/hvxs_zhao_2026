7fd6c93177929e1d2fb6a8f735ae4e07fa60de7a
Yue Zhao <zhao13@ualberta.ca>
2025-04-01 11:49:28 +0100

    Updated
    Updated to meet PEP 8

--- catalog/manipulate/parallax_zeropoint_correction.py
+++ catalog/manipulate/parallax_zeropoint_correction.py
@@ -6,8 +6,9 @@ from log.loggers import VerboseLogger
 
 def correct_zp(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
     """
-    Correct the parallax for zero-point offset. The corrected parallax column is named as "parallax_corr", it contains
-    the corrected parallax values when available, otherwise the original parallax values are used.
+    Correct the parallax for zero-point offset. The corrected parallax column
+    is named as "parallax_corr", it contains the corrected parallax values when
+    available, otherwise the original parallax values are used.
 
     Parameters
     ----------
@@ -20,12 +21,14 @@ def correct_zp(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
     Returns
     -------
     df : pd.DataFrame
-        The dataframe containing the corrected parallax, which is named as "parallax_corr".
+        The dataframe containing the corrected parallax, which is named as
+        "parallax_corr".
     """
+
     zpt.load_tables()
     logger = VerboseLogger(verbose=verbose)
 
-    logger.log(f"Catalogue loaded.\n")
+    logger.log("Catalogue loaded.\n")
     zero_point = df.apply(zpt.zpt_wrapper, axis=1)
 
     df[G.parallax_corr] = df[G.parallax] - zero_point
@@ -33,8 +36,11 @@ def correct_zp(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
     df[G.parallax_corr] = df[G.parallax_corr].fillna(df["parallax"])
     df[G.has_valid_parallax_corr] = has_valid_parallax_corr
 
-    logger.log(f"{sum(~has_valid_parallax_corr)} rows have NA parallax_corr, but they have valid parallaxes,"
-               f" so the parallax_corr is replaced with parallax values for these rows and are flagged by "
-               f"has_valid_parallax_corr.\n")
+    logger.log(
+        f"{sum(~has_valid_parallax_corr)} rows have NA parallax_corr, but "
+        "they have valid parallaxes, so the parallax_corr is replaced with "
+        "parallax values for these rows and are flagged by "
+        "has_valid_parallax_corr.\n"
+    )
 
     return df
