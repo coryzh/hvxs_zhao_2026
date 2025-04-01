414210e22a9b2227a6b4f08a1fbcc5973cbe6888
Yue Zhao <zhao13@ualberta.ca>
2025-04-01 12:00:58 +0100

    Updated
    Updated to meet PEP 8.

--- catalog/latex/make_simbad_type_tables.py
+++ catalog/latex/make_simbad_type_tables.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import config
 import data_schema as ds
-from pathlib import Path
 
 
 def remained_simbad_type_table(df: pd.DataFrame) -> None:
@@ -21,16 +20,21 @@ def remained_simbad_type_table(df: pd.DataFrame) -> None:
     """
 
     simbad_type_counts = df[ds.SimbadSchema.SIMBAD_MAIN_TYPE].value_counts()
-    type_names, type_counts = simbad_type_counts.index, simbad_type_counts.values
+    type_names = simbad_type_counts.index
+    type_counts = simbad_type_counts.values
     escaped_type_names = [item.replace("_", r"\_") for item in type_names]
     row_num = simbad_type_counts.shape[0] // 3
     row_str_list = []
 
     for row_index in range(row_num):
         # idx = 3 * row_index
-        row_str_single = (fr"{escaped_type_names[row_index]} & {type_counts[row_index]}"
-                          fr" & {escaped_type_names[row_index+8]} & {type_counts[row_index+8]}" 
-                          fr" & {escaped_type_names[row_index+16]} & {type_counts[row_index+16]} \\")
+        row_str_single = (
+            fr"{escaped_type_names[row_index]} & {type_counts[row_index]}"
+            fr" & {escaped_type_names[row_index+8]}"
+            fr" & {type_counts[row_index+8]}"
+            fr" & {escaped_type_names[row_index+16]}"
+            fr" & {type_counts[row_index+16]} \\"
+        )
 
         row_str_list.append(row_str_single)
 
@@ -38,16 +42,24 @@ def remained_simbad_type_table(df: pd.DataFrame) -> None:
 
     table_str = f"{table_start}{row_str}{table_end}"
 
-    out_file = config.RESULTS_LATEX_TABLE_DIR / "kept_simbad_types" / "kept_simbad_types.txt"
+    out_file = (
+        config.RESULTS_LATEX_TABLE_DIR
+        / "kept_simbad_types"
+        / "kept_simbad_types.txt"
+    )
+
     with open(out_file, "w") as f1:
         f1.write(table_str.strip())
 
-    # return table_str
-
 
 def main() -> None:
-    df = pd.read_csv(config.RESULTS_CATALOGUE_DIR / "high-v_sources" /
-                     "combined_vpec_lolim_gt_150_unique_w_simbad_high_ratio_simbad_cleaned_cleaned_cl_members.csv")
+    in_file = (
+        config.RESULTS_CATALOGUE_DIR
+        / "high-v_sources"
+        / "combined_vpec_lolim_gt_150_unique_w_simbad_high_"
+        "ratio_simbad_cleaned_cleaned_cl_members.csv"
+    )
+    df = pd.read_csv(in_file)
     remained_simbad_type_table(df)
 
 
