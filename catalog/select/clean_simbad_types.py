e746f89f04ea7961c1575c27fc513f3079150d99
Yue Zhao <zhao13@ualberta.ca>
2025-04-01 12:25:21 +0100

    Updated
    Updated to meet PEP 8.

--- catalog/select/clean_simbad_types.py
+++ catalog/select/clean_simbad_types.py
@@ -7,17 +7,22 @@ from log.loggers import VerboseLogger
 
 
 unwanted_otype_patterns = [
-    'Galaxy', 'Seyfert2', 'QSO', 'AGN', 'BLLac', 'Seyfert1', 'ClG', 'QSO_Candidate', "BrightestCG",
-    'AGN_Candidate', 'EmissionG', 'Blazar', 'Blazar_Candidate', 'RadioG', 'LINER', 'GtowardsCl',
-    'GtowardsCl', 'GinPair', 'Seyfert', 'BrownD*_Candidate', 'YSO', 'YSO_Candidate',
-    'GtowardsGroup', 'TTauri*', 'OrionV*', 'BrightestCG', 'PlanetaryNeb', 'LowSurfBrghtG',
-    'HIIG', 'OH/IR*', 'Supernova', 'Supernova_Candidate', 'TTauri*_Candidate', "HIIReg",
-    'InteractingG', 'SXPheV*', 'BLLac_Candidate', 'Ae*_Candidate', 'Ae*', 'CataclyV*', 'GlobCluster',
-    "Nova", "XrayBin", "LowMassXBin", "HighMassXBin", "Pulsar", '**', "ULX_Candidate", "alf2CVnV*",
-    "Planet_Candidate", "Cepheid", "Type2Cep", "ClassicalCep", "RSCVnV*", "LensedQ", "GravLens", "SB*",
-    "XrayBin_Candidate", "DarkNeb", "PartofCloud", "LowMassXBin_Candidate", "Bubble", "Association",
-    "Outflow_Candidate", "Outflow", "Cluster*", "Neutron*", "RSCVnV*_Candidate", "BYDraV*_Candidate",
-    "Ae*", "BlackHole_Candidate", "LensedImage", "HIshell", "LensedImage_Candidate", "Neutron*_Candidate"
+    'Galaxy', 'Seyfert2', 'QSO', 'AGN', 'BLLac', 'Seyfert1', 'ClG',
+    'QSO_Candidate', "BrightestCG", 'AGN_Candidate', 'EmissionG', 'Blazar',
+    'Blazar_Candidate', 'RadioG', 'LINER', 'GtowardsCl', 'GtowardsCl',
+    'GinPair', 'Seyfert', 'BrownD*_Candidate', 'YSO', 'YSO_Candidate',
+    'GtowardsGroup', 'TTauri*', 'OrionV*', 'BrightestCG', 'PlanetaryNeb',
+    'LowSurfBrghtG', 'HIIG', 'OH/IR*', 'Supernova', 'Supernova_Candidate',
+    'TTauri*_Candidate', "HIIReg", 'InteractingG', 'SXPheV*',
+    'BLLac_Candidate', 'Ae*_Candidate', 'Ae*', 'CataclyV*', 'GlobCluster',
+    "Nova", "XrayBin", "LowMassXBin", "HighMassXBin", "Pulsar", '**',
+    "ULX_Candidate", "alf2CVnV*", "Planet_Candidate", "Cepheid", "Type2Cep",
+    "ClassicalCep", "RSCVnV*", "LensedQ", "GravLens", "SB*",
+    "XrayBin_Candidate", "DarkNeb", "PartofCloud", "LowMassXBin_Candidate",
+    "Bubble", "Association", "Outflow_Candidate", "Outflow", "Cluster*",
+    "Neutron*", "RSCVnV*_Candidate", "BYDraV*_Candidate", "Ae*",
+    "BlackHole_Candidate", "LensedImage", "HIshell", "LensedImage_Candidate",
+    "Neutron*_Candidate"
 ]
 
 unwanted_otypes_patterns = [
@@ -35,7 +40,9 @@ unwanted_otypes_patterns = [
 unwanted_name_patterns = ["Cl*", "Cl", "NGC", "LEDA"]
 
 
-def _clean_df_simbad_main_type(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
+def _clean_df_simbad_main_type(
+        df: pd.DataFrame, verbose: bool = False
+) -> pd.DataFrame:
     """
     Clean the main type of the sources in the input DataFrame.
     Parameters
@@ -51,16 +58,28 @@ def _clean_df_simbad_main_type(df: pd.DataFrame, verbose: bool = False) -> pd.Da
     logger = VerboseLogger(verbose=verbose)
     logger.begin()
     logger.log(f"{df.shape[0]} rows loaded.\n")
-    _filter = ~df[ds.SimbadSchema.SIMBAD_MAIN_TYPE].apply(lambda x: any(types in x for types in unwanted_otype_patterns))
-    logger.log(f"{df.shape[0] - _filter.sum()} rows removed based on their main_type.\n")
+
+    _filter = (
+        ~df[ds.SimbadSchema.SIMBAD_MAIN_TYPE]
+        .apply(lambda x: any(types in x for types in unwanted_otype_patterns))
+    )
+
+    logger.log(
+        f"{df.shape[0] - _filter.sum()} rows removed based on their "
+        "main_type.\n"
+    )
+
     logger.end()
 
     return df[_filter]
 
 
-def _clean_df_simbad_secondary_types(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
+def _clean_df_simbad_secondary_types(
+        df: pd.DataFrame, verbose: bool = False
+) -> pd.DataFrame:
     """
-    Clean the catalogue based on secondary types of the sources in the input DataFrame.
+    Clean the catalogue based on secondary types of the sources in the input
+    DataFrame.
     Parameters
     ----------
     df : pd.DataFrame
@@ -77,17 +96,30 @@ def _clean_df_simbad_secondary_types(df: pd.DataFrame, verbose: bool = False) ->
     logger.log(f"{df.shape[0]} rows loaded.\n")
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        df.loc[:, ds.SimbadSchema.SIMBAD_OTYPES] = df[ds.SimbadSchema.SIMBAD_OTYPES].fillna('nan')
-        _filter = ~df[ds.SimbadSchema.SIMBAD_OTYPES].apply(lambda x: any(types in x for types
-                                                                         in unwanted_otypes_patterns))
+        df.loc[:, ds.SimbadSchema.SIMBAD_OTYPES] = (
+            df[ds.SimbadSchema.SIMBAD_OTYPES].fillna('nan')
+        )
+        _filter = (
+            ~df[ds.SimbadSchema.SIMBAD_OTYPES]
+            .apply(
+                lambda x: any(types in x for types in unwanted_otypes_patterns)
+            )
+        )
 
-    logger.log(f"{df.shape[0] - _filter.sum()} rows removed based on their secondary types.\n")
+    logger.log(
+        f"{df.shape[0] - _filter.sum()} rows removed based on their "
+        "secondary types.\n"
+    )
+
     logger.end()
 
     return df[_filter]
 
 
-def _clean_df_simbad_name(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
+def _clean_df_simbad_name(
+        df: pd.DataFrame, verbose: bool = False
+) -> pd.DataFrame:
+
     """
     Clean the names of the sources in the input DataFrame.
     Parameters
@@ -106,31 +138,56 @@ def _clean_df_simbad_name(df: pd.DataFrame, verbose: bool = False) -> pd.DataFra
     logger.log(f"{df.shape[0]} rows loaded.\n")
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        df.loc[:, ds.SimbadSchema.SIMBAD_ID] = df[ds.SimbadSchema.SIMBAD_ID].fillna('nan')
-        _filter = ~df[ds.SimbadSchema.SIMBAD_ID].apply(lambda x: any(types in x for types in unwanted_name_patterns))
 
-    logger.log(f"{df.shape[0] - _filter.sum()} rows removed based on their SIMBAD names.\n")
+        df.loc[:, ds.SimbadSchema.SIMBAD_ID] = (
+            df[ds.SimbadSchema.SIMBAD_ID].fillna('nan')
+        )
+
+        _filter = (
+            ~df[ds.SimbadSchema.SIMBAD_ID]
+            .apply(
+                lambda x: any(types in x for types in unwanted_name_patterns)
+            )
+        )
+
+    logger.log(
+        "{df.shape[0] - _filter.sum()} rows removed based on their "
+        "SIMBAD names.\n"
+    )
+
     logger.end()
 
     return df[_filter]
 
 
 def main() -> None:
-    in_file = (config.RESULTS_CATALOGUE_DIR / "high-v_sources"
-               / "combined_vpec_lolim_gt_150_unique_stage_2.csv")
+    in_file = (
+        config.RESULTS_CATALOGUE_DIR
+        / "high-v_sources"
+        / "combined_vpec_lolim_gt_150_unique_stage_2.csv"
+    )
+
     df = pd.read_csv(in_file)
-    df[ds.SimbadSchema.get_attribute_values()] = df[ds.SimbadSchema.get_attribute_values()].fillna("")
+    df[ds.SimbadSchema.get_attribute_values()] = (
+        df[ds.SimbadSchema.get_attribute_values()].fillna("")
+    )
 
     verbose = True
+
     df_filtered_1 = _clean_df_simbad_main_type(df, verbose=verbose)
 
-    df_filtered_2 = _clean_df_simbad_secondary_types(df_filtered_1, verbose=verbose)
+    df_filtered_2 = _clean_df_simbad_secondary_types(
+        df_filtered_1, verbose=verbose
+    )
 
     df_filtered_3 = _clean_df_simbad_name(df_filtered_2, verbose=verbose)
 
-    df_filtered_3.to_csv(in_file.parent / f"{in_file.stem.replace('stage_2', 'stage_3')}.csv", index=False)
+    df_filtered_3.to_csv(
+        in_file.parent / f"{in_file.stem.replace('stage_2', 'stage_3')}.csv",
+        index=False
+    )
 
-    print(f"Done!")
+    print("Done!")
 
 
 if __name__ == "__main__":
