61869db9c56c6a1493091d72a5626e3c5b70fa7a
Yue Zhao <zhao13@ualberta.ca>
2025-04-01 13:42:37 +0100

    Added
    This script is used to select sources that have space velocities above the expected escape velocity.

---
+++ catalog/select/select_runaway_sources.py
@@ -0,0 +1,43 @@
+import config
+import pandas as pd
+from potential import MW
+from galpy.potential import vesc
+from pathlib import Path
+from astropy.units import Unit
+
+
+def select_runaway(in_file: Path) -> None:
+    df = pd.read_csv(in_file)
+
+    r_gc = df["r_gc"].values * Unit("kpc")
+
+    v_escape = vesc(MW, r_gc)
+
+    v_space_min_lolim = df["vspace_min_med"] - df["e_vspace_min"]
+
+    _filter = (
+        (v_space_min_lolim >= v_escape.value)
+        & (df["distance_inference"] != "fixed_at_10")
+    )
+
+    df_filtered = df[_filter]
+
+    out_file = (
+        in_file.parent.parent
+        / "runaway_sources"
+        / f"{in_file.stem}_runaway.csv"
+    )
+    df_filtered.to_csv(out_file, index=False)
+
+
+def main() -> None:
+    in_file = (
+        config.RESULTS_CATALOGUE_DIR
+        / "high-v_sources"
+        / "combined_vpec_lolim_gt_150_unique_stage_9.csv"
+    )
+    select_runaway(in_file)
+
+
+if __name__ == "__main__":
+    main()
