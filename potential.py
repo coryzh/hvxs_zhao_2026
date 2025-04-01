f15e76ccf3a7acc631348969391062fe7b672961
Yue Zhao <zhao13@ualberta.ca>
2025-04-01 10:54:20 +0100

    Update
    Minor update on constant naming. Constants should be all-cap following the convention.

--- potential.py
+++ potential.py
@@ -7,4 +7,4 @@ vo = con.Theta_0
 PSP = PowerSphericalPotentialwCutoff(alpha=1.8, rc=1.9 / 8., normalize=0.05, ro=ro, vo=vo)
 MYP = MiyamotoNagaiPotential(a=3. / 8., b=0.28 / 8., normalize=.6, ro=ro, vo=vo)
 NFW = NFWPotential(a=16 / 8., normalize=.35, ro=ro, vo=vo)
-mw = PSP + MYP + NFW
+MW = PSP + MYP + NFW
