d9d499561b0fe9849e69888e6e5932396e38760f
Yue Zhao <zhao13@ualberta.ca>
2025-04-09 10:26:20 +0100

    Updated
    Updated to meet PEP 8.

--- utils/rotation_curve.py
+++ utils/rotation_curve.py
@@ -5,6 +5,7 @@ from scipy.interpolate import interp1d
 import constants as con
 import astropy.units as u
 import numpy as np
+from numba import jit
 
 # This python script extract rotation curve data from the galpy MWPotential2014
 
@@ -13,12 +14,22 @@ vo = con.Theta_0
 
 
 def calc_v_rot(R):
-    psp = PowerSphericalPotentialwCutoff(alpha=1.8, rc=1.9 / 8., normalize=0.05, ro=ro, vo=vo)
-    myp = MiyamotoNagaiPotential(a=3. / 8., b=0.28 / 8., normalize=.6, ro=ro, vo=vo)
-    nfw = NFWPotential(a=16 / 8., normalize=.35, ro=ro, vo=vo)
+    psp = PowerSphericalPotentialwCutoff(
+        alpha=1.8, rc=1.9 / 8., normalize=0.05, ro=ro, vo=vo
+    )
+    myp = MiyamotoNagaiPotential(
+        a=3. / 8., b=0.28 / 8., normalize=.6, ro=ro, vo=vo
+    )
+    nfw = NFWPotential(
+        a=16 / 8., normalize=.35, ro=ro, vo=vo
+    )
     mw = psp + myp + nfw
 
-    v_rot_galpy = calcRotcurve(mw, Rs=R * u.kpc) * conversion.velocity_in_kpcGyr(240, 8.34) * (u.kpc / u.Gyr).to(u.km / u.s)
+    v_rot_galpy = (
+        calcRotcurve(mw, Rs=R * u.kpc)
+        * conversion.velocity_in_kpcGyr(240, 8.34)
+        * (u.kpc / u.Gyr).to(u.km / u.s)
+    )
 
     return v_rot_galpy
 
@@ -31,5 +42,15 @@ for i, r_val in enumerate(r_grid):
         v_grid[i] = calc_v_rot([0.01])[0]
     else:
         v_grid[i] = calc_v_rot([r_val])[0]
+
+
+@jit(nopython=True)
+def v_rot_nearest(x):
+    x = np.asarray(x)
+
+    idx = np.abs(x[:, np.newaxis] - r_grid).argmin(axis=1)
+
+    return v_grid[idx]
+
 
 v_rot = interp1d(r_grid, v_grid, bounds_error=False, fill_value=v_grid[-1])
