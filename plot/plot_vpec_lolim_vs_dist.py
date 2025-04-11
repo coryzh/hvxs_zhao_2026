d571de7fb3acf35036cfb1c9c1e9dd8ea73d4f34
Yue Zhao <zhao13@ualberta.ca>
2025-04-11 15:29:22 +0100

    Updated
    Reworked on colorbar:
    
    (1) Colorbar is positionsed in the upper-right panel.
    
    (2) Right and left panel scatter share the colormap.

--- plot/plot_vpec_lolim_vs_dist.py
+++ plot/plot_vpec_lolim_vs_dist.py
@@ -4,9 +4,11 @@ import config
 import plot.plot_settings as ps
 import numpy as np
 from utils.process_string import get_short_id
-from matplotlib import colors
 from typing import Tuple
 from astropy.units import Unit
+from matplotlib.ticker import LogLocator
+from matplotlib.colors import LogNorm
+from matplotlib import cm
 
 
 def make_figure() -> Tuple[plt.Figure, dict]:
@@ -23,11 +25,13 @@ def make_figure() -> Tuple[plt.Figure, dict]:
     ax_main = fig.add_subplot(gs[1, 0])
     ax_right = fig.add_subplot(gs[1, 1], sharey=ax_main)
     ax_top = fig.add_subplot(gs[0, 0], sharex=ax_main)
+    ax_cbar = fig.add_subplot(gs[0, 1])
 
     ax_dict = {
         "main": ax_main,
         "right": ax_right,
-        "top": ax_top
+        "top": ax_top,
+        "cbar": ax_cbar
     }
 
     return fig, ax_dict
@@ -38,6 +42,7 @@ def axes_settings(ax_dict: dict) -> None:
     ax_main = ax_dict["main"]
     ax_right = ax_dict["right"]
     ax_top = ax_dict["top"]
+    ax_cbar = ax_dict["cbar"]
 
     ax_main.set_xlabel(r"$d$ (kpc)")
     ax_main.set_ylabel(r"$v_\mathrm{pec, min, lo}\,(\mathrm{km~s^{-1}})$")
@@ -59,10 +64,20 @@ def axes_settings(ax_dict: dict) -> None:
     ax_top.tick_params(axis='y', labelleft=False)
 
     ax_right.set_xlabel(r"$\sqrt{2}d\epsilon$ (AU)")
-    ax_right.tick_params(axis='y', left=False, labelleft=False)
+    ax_right.tick_params(axis='y', labelleft=False)
     ax_right.set_xlim(5e-4, 300)
     ax_right.set_xticks([0.001, 0.01, 0.1, 1, 10, 100])
     ax_right.set_xticklabels([r"$10^{-3}$", "0.01", "0.1", "1", "10", "100"])
+    ax_right.set_yticks([10, 100, 1000, 2000])
+
+    for pos in ["top", "left", "bottom", "right"]:
+        ax_cbar.spines[pos].set_visible(False)
+
+    ax_cbar.tick_params(
+        axis="both", which="both",
+        bottom=False, top=False, left=False, right=False,
+        labelbottom=False, labelleft=False
+    )
 
 
 def add_prime_sources(df_prime: pd.DataFrame, ax: list[plt.Axes]) -> None:
@@ -87,29 +102,16 @@ def add_prime_sources(df_prime: pd.DataFrame, ax: list[plt.Axes]) -> None:
 
 def add_control_and_hvxs(
         ax: plt.Axes, fig: plt.Figure,
-        df_hvxs: pd.DataFrame, df_control: pd.DataFrame) -> None:
-
-    # min_aen_val = 1e-3
-    df_hvxs["parallax_over_error"] = (
-        abs(df_hvxs["parallax_corr"]) / df_hvxs["parallax_error"]
-    )
-    # df_hvxs = df_hvxs.sort_values(
-    #     by="parallax_over_error",
-    #     ascending=False
-    # )
-
-    # df_hvxs["astrometric_excess_noise"] = (
-    #     df_hvxs["astrometric_excess_noise"]
-    #     .replace(0, min_aen_val)
-    # )
+        df_hvxs: pd.DataFrame, df_control: pd.DataFrame, sm: cm.ScalarMappable
+) -> None:
 
     c = df_hvxs["parallax_over_error"]
 
     scatter_style = {
         "HVXS": {
             "s": 30, "ec": "k", "c": c, "alpha": 0.8, "rasterized": True,
-            "label": "HVXS", "cmap": ps.CMAP,
-            "norm": colors.LogNorm()
+            "label": "HVXS", "cmap": sm.cmap,
+            "norm": sm.norm
         },
         "Control": {
             "s": 0.1, "c": "k", "alpha": 0.5, "rasterized": True,
@@ -126,17 +128,7 @@ def add_control_and_hvxs(
         x = df["dist_med"]
         y = df["vpec_min_med"] - df["e_vpec_min"]
         if key == "HVXS":
-            scatter = ax.scatter(x, y, **scatter_style[key])
-            cax = ax.inset_axes([0.06, 0.85, 0.4, 0.05])
-            _cbar = fig.colorbar(scatter, cax=cax, orientation="horizontal")
-            _cbar.set_label(r"$|\varpi|/\sigma_\varpi$", labelpad=10,
-                            size=20)
-
-            _cbar.set_ticks([0.001, 0.1, 10])
-            _cbar.set_ticklabels([
-                0.001, 0.1, 10
-            ], size=18)
-            _cbar.ax.xaxis.set_label_position("top")
+            ax.scatter(x, y, **scatter_style[key])
         else:
             ax.scatter(x, y, **scatter_style[key])
 
@@ -180,7 +172,8 @@ def add_top_hist(
 
 
 def add_right_panel(
-        ax_right: plt.Axes, df_hvxs: pd.DataFrame, df_control: pd.DataFrame
+        ax_right: plt.Axes, fig: plt.Figure,
+        df_hvxs: pd.DataFrame, df_control: pd.DataFrame, sm: cm.ScalarMappable
 ) -> None:
 
     df_dict = {
@@ -188,16 +181,13 @@ def add_right_panel(
         "Control": df_control
     }
 
-    df_hvxs["parallax_over_error"] = (
-        abs(df_hvxs["parallax_corr"]) / df_hvxs["parallax_error"]
-    )
-
     c = df_hvxs["parallax_over_error"].values
 
     scatter_style = {
         "HVXS": {
             "s": 30, "ec": "k", "c": c, "alpha": 0.8, "rasterized": True,
-            "label": "HVXS", "cmap": ps.CMAP, "norm": colors.LogNorm()
+            "label": "HVXS", "cmap": sm.cmap,
+            "norm": sm.norm
         },
         "Control": {
             "s": 0.1, "c": "k", "alpha": 0.5, "rasterized": True,
@@ -215,8 +205,38 @@ def add_right_panel(
 
         y = df["vpec_min_med"] - df["e_vpec_min"]
 
-        ax_right.scatter(x, y, **scatter_style[key])
+        if key == "HVXS":
+            ax_right.scatter(x, y, **scatter_style[key])
+        else:
+            ax_right.scatter(x, y, **scatter_style[key])
     ax_right.axhline(y=150, ls=":", color="k")
+
+
+def add_cbar(ax: plt.Axes, fig: plt.Figure, df: pd.DataFrame,
+             sm: cm.ScalarMappable) -> None:
+
+    cax = ax.inset_axes([0.025, 0.15, 0.95, 0.2])
+    _cbar = fig.colorbar(sm, cax=cax, orientation="horizontal")
+    _cbar.set_label(
+        r"$|\varpi|/\sigma_\varpi$", labelpad=10, size=20
+    )
+    _cbar.ax.xaxis.set_label_position("top")
+
+    _cbar.ax.set_xscale('log')
+
+    _cbar.set_ticks([1e-4, 0.001, 0.01, 0.1, 1, 10])
+    _cbar.set_ticklabels(
+        [
+            r"$10^{-4}$", "0.001", "0.01", "0.1", "1", "10"
+        ], size=18
+    )
+
+    _cbar.ax.xaxis.set_minor_locator(
+        LogLocator(base=10.0, subs=np.arange(0.1, 1, 0.1), numticks=12)
+    )
+    # _cbar.ax.xaxis.set_minor_formatter(
+    #     FormatStrFormatter("%.1f")
+    # )  # Optional: Format minor tick labels
 
 
 def make_plot() -> None:
@@ -230,18 +250,32 @@ def make_plot() -> None:
         / "control_sample" / "control_sample_stage_10.csv"
     )
 
-    fig, ax_dict = make_figure()
-    axes_settings(ax_dict)
-    add_control_and_hvxs(
-        df_hvxs=df_hvxs, df_control=df_control, ax=ax_dict["main"], fig=fig
+    df_hvxs["parallax_over_error"] = (
+        abs(df_hvxs["parallax_corr"]) / df_hvxs["parallax_error"]
     )
 
-    add_top_hist(
-        ax_dict["top"], df_hvxs=df_hvxs, df_control=df_control
+    poe = df_hvxs["parallax_over_error"]
+    sm_lognorm = cm.ScalarMappable(norm=LogNorm(
+        vmin=poe.min(), vmax=poe.max()), cmap=ps.CMAP
+    )
+
+    fig, ax_dict = make_figure()
+    axes_settings(ax_dict)
+
+    add_control_and_hvxs(
+        df_hvxs=df_hvxs, df_control=df_control, ax=ax_dict["main"], fig=fig,
+        sm=sm_lognorm
     )
 
     add_right_panel(
-        ax_dict["right"], df_hvxs=df_hvxs, df_control=df_control
+        ax_dict["right"], fig, df_hvxs=df_hvxs, df_control=df_control,
+        sm=sm_lognorm
+    )
+
+    add_cbar(ax=ax_dict["cbar"], df=df_hvxs, fig=fig, sm=sm_lognorm)
+
+    add_top_hist(
+        ax_dict["top"], df_hvxs=df_hvxs, df_control=df_control
     )
 
     plt.savefig(
