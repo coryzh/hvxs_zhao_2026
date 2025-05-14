89efc5197b11e69a4e1be7c7038d267af3202747
Yue Zhao <zhao13@ualberta.ca>
2025-05-14 19:43:39 +0100

    Updated
    Format updates to meet PEP 8.

--- query/gaia.py
+++ query/gaia.py
@@ -5,14 +5,17 @@ from astropy.table import Table
 
 Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 default_columns = [
-    "designation", "source_id", "ra", "ra_error", "dec", "dec_error", "parallax", "parallax_error",
-    "pmra", "pmra_error", "pmdec", "pmdec_error", "phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag",
-    "nu_eff_used_in_astrometry", "pseudocolour", "ecl_lat", "astrometric_params_solved", "ag_gspphot",
-    "ebpminrp_gspphot", "azero_gspphot"
+    "designation", "source_id", "ra", "ra_error", "dec", "dec_error",
+    "parallax", "parallax_error", "pmra", "pmra_error", "pmdec", "pmdec_error",
+    "phot_g_mean_mag", "phot_bp_mean_mag", "phot_rp_mean_mag",
+    "nu_eff_used_in_astrometry", "pseudocolour", "ecl_lat",
+    "astrometric_params_solved", "ag_gspphot", "ebpminrp_gspphot",
+    "azero_gspphot"
 ]
 
 
-def gaia_single_source_id_search(source_id: int, columns: List[str] = None) -> Table:
+def gaia_single_source_id_search(source_id: int,
+                                 columns: List[str] = None) -> Table:
     if columns is None:
         columns = default_columns
 
