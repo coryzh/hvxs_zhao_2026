ecba0d2620814d0e21675556c4aa8e170a7cd99e
Yue Zhao <zhao13@ualberta.ca>
2025-05-16 19:26:34 +0100

    Updated
    Added the gamma_min column to the table and fixed several small mistakes.

--- catalog/latex/make_sample_table.py
+++ catalog/latex/make_sample_table.py
@@ -6,19 +6,20 @@ from utils.process_string import wrap_sign
 
 
 def make_prime_sample_source_table(df: pd.DataFrame) -> None:
+    df = df.sort_values(by="ra_x", ascending=True)
     table_start = r"""
     \begin{table*}
+    \caption{A sample table of curated runaway sources (see Sect \ref{sec:prime-sample}).}
     \centering
     \begin{tabular}{llccccc}
     \hline
-    X-ray ID & Gaia DR3 & Separation  & \gaia\ G & d & $\vpecmin$ & $\vspacemin$ \\
+    X-ray ID & Gaia DR3 & Separation  & \gaia\ G & $\distance$ & $\vpecmin$ & $\vpecgammamin$ \\
              &          & ($\sigma$)  &          & ($\kpc$) & $(\kms)$ & $(\kms)$ \\
     \hline
     """
 
     table_end = r"""
         \hline
-        \caption{A sample table of curated runaway sources (see Sect \ref{sec:prime-sample}).}
         \end{tabular}
         \label{tab:sample-table-hvxs}
     \end{table*}
@@ -29,7 +30,6 @@ def make_prime_sample_source_table(df: pd.DataFrame) -> None:
         id_g_str = f"{row['source_id']}"
         sep_str = f"{row['sep_x_g'] / row['pos_x_err']:.1f} "
         gmag_str = f"{row['phot_g_mean_mag']:.2f} "
-
         id_x_str = wrap_sign(id_x_str)
         d_str = (
             f"${row['dist_med']:.1f}"
@@ -43,16 +43,22 @@ def make_prime_sample_source_table(df: pd.DataFrame) -> None:
             rf"_{{-{row['e_vpec_min']:.1f}}}$"
         )
 
-        vspace_str = (
-            rf"${row['vspace_min_med']:.1f}"
-            rf"^{{+{row['E_vspace_min']:.1f}}}"
-            rf"_{{-{row['e_vspace_min']:.1f}}}$ \\"
+        # vspace_str = (
+        #     rf"${row['vspace_min_med']:.1f}"
+        #     rf"^{{+{row['E_vspace_min']:.1f}}}"
+        #     rf"_{{-{row['e_vspace_min']:.1f}}}$ \\"
+        # )
+
+        vpec_gamma_str = (
+            rf"${row['vpec_gamma_min_med']:.1f}"
+            rf"^{{+{row['E_vpec_gamma_min']:.1f}}}"
+            rf"_{{-{row['e_vpec_gamma_min']:.1f}}}$ \\"
         )
 
         row_str = " & ".join(
             [
                 id_x_str, id_g_str, sep_str, gmag_str, d_str,
-                vpec_str, vspace_str
+                vpec_str, vpec_gamma_str
             ]
         )
         row_list.append(row_str)
@@ -77,7 +83,7 @@ def make_prime_sample_source_table(df: pd.DataFrame) -> None:
 def main() -> None:
     df = pd.read_csv(config.RESULTS_CATALOGUE_DIR
                      / "prime_sample"
-                     / "gold_sample_150525.csv")
+                     / "gold_sample_150525_curated.csv")
 
     make_prime_sample_source_table(df)
 
