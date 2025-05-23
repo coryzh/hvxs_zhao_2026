fa14f7df6d45d71b74bfa070b86e2d95914c5e95
Yue Zhao <zhao13@ualberta.ca>
2025-05-23 13:08:47 +0100

    Major changes
    Reworked the script to plot alll HVXSs and gold samples in one figure. This is to distinguish it from the other script that plot the sources in 4 separate panels.

--- plot/plot_fxfg_vs_bprp_targets.py
+++ plot/plot_fxfg_vs_bprp_targets.py
@@ -2,62 +2,121 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import config
 import numpy as np
-from pathlib import Path
+import plot.plot_settings as ps
+from typing import Tuple, Any
+from utils.process_string import get_short_id
+from matplotlib.lines import Line2D
 from matplotlib.ticker import ScalarFormatter
 
 
-def make_figure(df: pd.DataFrame, out_file: Path = None) -> None:
+def make_figure() -> Tuple[plt.Axes, plt.Figure]:
     plt.style.use("mycustomised")
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    fig, ax = plt.subplots(1, 1, figsize=(12, 12))
 
-    df_all = pd.read_csv(
-        config.RESULTS_CATALOGUE_DIR
-        / "high-v_sources" / "combined_vpec_lolim_gt_150.0.csv"
-    )
+    return fig, ax
 
-    x_all = df_all["bp_rp"]
-    y_all = df_all["fx_fg"]
-    x_target = df["bp_rp"]
-    y_target = df["fx_fg"]
-    y_target_err = df["fx_fg_err"]
 
-    ax.scatter(x_all, y_all, s=0.6, marker=".", fc="k", ec="k")
-    ax.errorbar(
-        x_target, y_target, yerr=y_target_err, marker="o", ms=13.0,
-        mfc="limegreen", mec="k",
-        ls="none", ecolor="k", capsize=3
-    )
-
+def axes_settings(ax: plt.Axes) -> None:
     ax.set_yscale("log")
     ax.get_yaxis().set_major_formatter(ScalarFormatter())
-    ax.set_yticks([1e-5, 1e-4, 0.001, 0.01, 0.1, 1, 10])
+    ax.set_yticks([1e-5, 1e-4, 0.001, 0.01, 0.1, 1, 10, 100, 1000])
     ax.set_yticklabels(
-        [r"$10^{-5}$", r"$10^{-4}$", "0.001", "0.01", "0.1", "1", "10"]
+        [
+            r"$10^{-5}$", r"$10^{-4}$",
+            "0.001", "0.01", "0.1", "1", "10", "100", "1000"
+        ]
     )
 
+    ax.set_xlim(-0.8, 5.0)
+    ax.set_ylim(6e-6, 1050)
+    ax.set_xlabel("Bp$-$Rp", fontsize=38)
+    ax.set_ylabel("$F_X / F_G$", fontsize=38)
+
+
+def add_control(ax: plt.axes) -> None:
+    df_control = pd.read_csv(
+        config.RESULTS_CATALOGUE_DIR
+        / "control_sample" / "control_sample_stage_10.csv"
+    )
+
+    x = df_control["bp_rp"]
+    y = df_control["fx_fg"]
+
+    ax.scatter(
+        x, y, s=0.1, marker=".", c="k", alpha=0.2, rasterized=True, zorder=0
+    )
+
+
+def add_hvxs(df: pd.DataFrame, ax: plt.Axes) -> None:
+    x = df["bp_rp"]
+    y = df["fx_fg"]
+
+    ax.scatter(
+        x, y, s=20, marker="x", c="green", alpha=0.4, zorder=1, label="HVXS"
+    )
+
+
+def add_gold(df: pd.DataFrame, ax: plt.Axes) -> Any:
+    df = df.sort_values(by="ra_x", ascending=True)
+    marker_styles = ps.generate_marker_styles(
+        df.shape[0], generate_for="scatter"
+    )
+
+    for i, row in df.iterrows():
+        x = row["bp_rp"]
+        y = row["fx_fg"]
+        name = get_short_id(row["ID_x"])
+
+        ax.scatter(
+            x, y, label=name, zorder=2, s=150, ec="k", **marker_styles[i]
+        )
+
+    handles, labels = ax.get_legend_handles_labels()
+
+    return handles, labels
+
+
+def add_separatrix(ax: plt.Axes) -> None:
     bp_rp_min, bp_rp_max = ax.get_xlim()
     bp_rp_line = np.linspace(bp_rp_min, bp_rp_max, 100)
     fxfg_line = 10 ** (bp_rp_line - 3.5)
-    ax.plot(bp_rp_line, fxfg_line, lw=2.5, color="r", dashes=(5, 8))
+    ax.plot(bp_rp_line, fxfg_line, lw=2.5, color="r", label="R24")
 
-    ax.set_xlim(-0.8, 5.0)
-    ax.set_xlabel("Bp$-$Rp")
-    ax.set_ylabel("$F_X / F_G$")
 
-    if out_file is not None:
-        plt.savefig(out_file)
+def add_legend(handles, labels) -> None:
+    handle_HVXS = Line2D([0], [0], marker="x", color="green", ms=10,
+                         ls="none", mew=2.0)
+    handles[0] = handle_HVXS
+
+    plt.legend(
+        handles, labels, loc="upper left", bbox_to_anchor=(1.0, 1.0),
+        borderaxespad=0.0
+    )
 
 
 def main() -> None:
-    df_targets = pd.read_csv(config.RESULTS_CATALOGUE_DIR / "high-v_sources"
-                             / "vlt_p115" / "vlt_p115_targets_curated.csv")
-    out_file = (
-        config.RESULTS_FIGURES_DIR
-        / "high-v_sources" / "vlt_p115" / "fxfg_vs_bprp.pdf"
+    fig, ax = make_figure()
+
+    df_hvxs = pd.read_csv(
+        config.RESULTS_CATALOGUE_DIR / "high-v_sources"
+        / "hvxs_vpec_lo_gt_200.csv"
     )
 
-    make_figure(
-        df_targets, out_file=out_file
+    df_gold = pd.read_csv(
+        config.RESULTS_CATALOGUE_DIR / "prime_sample"
+        / "gold_sample_210525.csv"
+    )
+
+    add_hvxs(df_hvxs, ax)
+    add_control(ax)
+    handles, labels = add_gold(df_gold, ax)
+    add_separatrix(ax)
+    axes_settings(ax)
+    add_legend(handles, labels)
+
+    plt.savefig(
+        config.RESULTS_FIGURES_DIR
+        / "fxfg_vs_bprp" / "fxfg_vs_bprp.pdf"
     )
 
 
