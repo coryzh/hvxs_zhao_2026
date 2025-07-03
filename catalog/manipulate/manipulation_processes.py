34a98f0097f61c28657b2289d44e1937bf680012
Yue Zhao <zhao13@ualberta.ca>
2025-07-03 19:15:49 +0100

    Added function
    Added function to add fidelity columns to the control sample. Because there is a row limit in the German Virtual Observatory TAP service, I chunk up the control sample table, queried the service, and merge the chunks here

--- catalog/manipulate/manipulation_processes.py
+++ catalog/manipulate/manipulation_processes.py
@@ -4,6 +4,35 @@ from log.loggers import VerboseLogger
 from typing import List
 from pathlib import Path
 import data_schema as ds
+from glob import glob
+
+
+def add_fidelity_columns() -> None:
+    in_root = config.RESULTS_CATALOGUE_DIR / "control_chunks"
+    in_files = glob(
+        str(in_root / "*_fidelity.csv")
+    )
+
+    dfs = []
+    for file_path in in_files:
+        dfs.append(
+            pd.read_csv(file_path)
+        )
+
+    df_fidelity = pd.concat(dfs, ignore_index=True)
+
+    df_control = pd.read_csv(
+        config.RESULTS_CATALOGUE_DIR / "ready_catalogues" / "control.csv"
+    )
+
+    df_merged = pd.merge(
+        df_control, df_fidelity, on="source_id", how="left"
+    )
+
+    df_merged.to_csv(
+        config.RESULTS_CATALOGUE_DIR / "ready_catalogues"
+        / "control_w_fidelity.csv"
+    )
 
 
 def make_master(
@@ -141,6 +170,8 @@ def main() -> None:
     for s in survey_names:
         schema = ds.SCHEMA_DICT[s]
         make_master(survey_name=s, id_x_col=schema.ID, verbose=True)
+
+    # add_fidelity_columns()
 
 
 if __name__ == "__main__":
