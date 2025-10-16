ee77b53dc76439c0b526da4af71a22b00e75859f
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-10-16 13:40:01 +0100

    Completed a version of the check_overlap function

--- catalog/auto_correlate_xray_catalogues.py
+++ catalog/auto_correlate_xray_catalogues.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import config
 import logging
+import numpy as np
+import time
 from log.log_config import configure_logging
 from pathlib import Path
 
@@ -73,8 +75,67 @@ def _concatenate_catalogues() -> pd.DataFrame:
     return df_all
 
 
+def check_overlap(df: pd.DataFrame) -> None:
+    logger.info(f"Input DataFrame has {df.shape[0]} rows")
+    n_tot = df.shape[0]
+
+    df_sep = pd.DataFrame(
+        columns=["id_x_1", "id_x_2", "sep", "pos_x_err_1", "pos_x_err_2"]
+    )
+
+    logger.info("Checking for overlapping sources...")
+    for i in range(n_tot):
+        for j in range(i + 1, n_tot):
+            ra_x1 = df.loc[i, "ra_x"]
+            dec_x1 = df.loc[i, "dec_x"]
+            ra_x2 = df.loc[j, "ra_x"]
+            dec_x2 = df.loc[j, "dec_x"]
+            pos_x_err_1 = df.loc[i, "pos_x_err"]
+            pos_x_err_2 = df.loc[j, "pos_x_err"]
+            id_x_1 = df.loc[i, "ID_x"]
+            id_x_2 = df.loc[j, "ID_x"]
+            sep = np.sqrt(
+                (ra_x1 - ra_x2) ** 2 +
+                (dec_x1 - dec_x2) ** 2
+            )
+
+            err_sum = df.loc[i, "pos_x_err"] + df.loc[j, "pos_x_err"]
+            if sep < err_sum:
+                row = pd.Series(
+                    {
+                        "id_x_1": id_x_1,
+                        "id_x_2": id_x_2,
+                        "sep": sep,
+                        "pos_x_err_1": pos_x_err_1,
+                        "pos_x_err_2": pos_x_err_2
+                    }
+                )
+
+                df_sep = pd.concat(
+                    [df_sep, row.to_frame().T], ignore_index=True
+                )
+
+    n_overlap = df_sep.shape[0]
+    print(df_sep)
+    logger.info(
+        f"Found {n_overlap} overlapping sources.",
+    )
+
+
 if __name__ == "__main__":
     logger = logging.getLogger(Path(__file__).stem)
     configure_logging(level=logging.INFO, app_name=Path(__file__).stem)
 
-    _ = _concatenate_catalogues()
+    df_all = _concatenate_catalogues()
+
+    start_time = time.perf_counter()
+    df_test = df_all.sample(n=1000, random_state=42).reset_index(drop=True)
+    check_overlap(df_test)
+    elapsed = time.perf_counter() - start_time
+
+    expected_hours = (elapsed / df_test.shape[0]) * df_all.shape[0] / 3600
+    logger.info(
+        f"Checking on {df_test.shape[0]} rows completed in {elapsed:.2f} "
+        f"seconds. For the full catalogue with {df_all.shape[0]} rows, "
+        f"it may take up to {expected_hours:.2f} hours."
+    )
