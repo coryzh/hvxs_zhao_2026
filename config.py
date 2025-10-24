8f7825da19df1316b69188e79ff330967c7d834f
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-10-24 13:57:29 +0100

    Add SURVEY_ID_IDENTIFIERS dictionary for survey identification

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ from pathlib import Path
 
 SURVEY_NAME = "combined"
 SURVEY_NAMES_SHORT = ["csc", "xmm", "erass", "swift"]
-
+SURVEY_ID_IDENTIFIERS = {
+    "2CXO": "csc",
+    "4XMM": "xmm",
+    "1eRASS": "erass",
+    "2SXPS": "swift"
+}
 # Path to the root of the project
 ROOT_DIR = Path(__file__).parent.parent
 
