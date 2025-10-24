01206ed2ec7b8d6031995ac51819181ddc9e124f
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-10-24 17:53:42 +0100

    Updated
    Added scripts for processing the catalogues of Gaia neighbours.

--- catalog/for_revision/prepare_catalogues_for_nway.sh
+++ catalog/for_revision/prepare_catalogues_for_nway.sh
@@ -12,14 +12,21 @@ declare -A SKY_AREA=(
 )
 
 for survey in "${SURVEYS[@]}"; do
-    FILE_IN="../../../results/combined/catalogues/x_ray_catalogue_for_nway/xray_catalogue_${survey}.csv"
-
+    FILE_XRAY_IN="../../../results/combined/catalogues/x_ray_catalogue_for_nway/xray_catalogue_${survey}.csv"
+    FILE_GAIA_IN="../../../results/combined/catalogues/x_ray_catalogue_for_nway/${survey}_gaia_neighbours.csv"
         # Run the command
-        python3 prepare_catalogue_for_nway.py "$FILE_IN" \
+        python3 prepare_catalogue_for_nway.py "$FILE_XRAY_IN" \
             --id_col "ID_x" \
             --ra_col "ra_x" \
             --dec_col "dec_x" \
             --pos_err_col "pos_x_err" \
             --sky_area "${SKY_AREA[$survey]}" \
             --data_ext_name "$survey"
+
+        python3 prepare_catalogue_for_nway.py "$FILE_GAIA_IN" \
+            --id_col "source_id" \
+            --ra_col "ra" \
+            --dec_col "dec" \
+            --sky_area 41253.0 \
+            --data_ext_name "Gaia"
     done
