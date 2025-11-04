84c48d3ea856c5bf3f6184a8eecddfac865d6060
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-11-04 19:07:13 +0000

    Refactor histogram plotting script to include data loading and processing functions, enhance logging, and streamline histogram generation

--- plot/plot_sep_x_g_histogram.py
+++ plot/plot_sep_x_g_histogram.py
@@ -2,9 +2,52 @@ import numpy as np
 import config
 import pandas as pd
 import matplotlib.pyplot as plt
+import data_schema as ds
 from plot.plot_settings import SURVEY_COLOR_DICT_X, SURVEY_NAME_DICT_X
 from typing import Tuple
 from scipy.stats import gaussian_kde
+from log.log_config import configure_logging
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
+configure_logging(level=logging.INFO, app_name=Path(__file__).stem)
+
+
+def _add_from_colunmn(df: pd.DataFrame) -> pd.DataFrame:
+    df_copy = df.copy()
+    id_x_col = ds.CombinedCatalogueSchema.ID_x
+    df_copy["from"] = (
+        df_copy[id_x_col]
+        .astype(str)
+        .str
+        .split(' ')
+        .str[0]
+    ).map(config.SURVEY_ID_IDENTIFIERS)
+    logger.info("Added 'from' column to the DataFrame.")
+
+    return df_copy
+
+
+def _load_catalogue() -> pd.DataFrame:
+    in_file = (
+        config.RESULTS_CATALOGUE_DIR
+        / "nway_matched_results"
+        / "xray_catalogue_concatenated_deduplicated.csv"
+    )
+
+    df = pd.read_csv(in_file)
+    logger.info(
+        f"Loaded deduplicated concatenated X-ray catalogue from {in_file} "
+        f"with {df.shape[0]} sources."
+    )
+
+    return df
+
+
+def _save_to_figure(out_file: Path) -> None:
+    plt.savefig(out_file)
+    logger.info(f"Saved figure to {out_file}.")
 
 
 def make_figure() -> Tuple[plt.Figure, plt.Axes]:
@@ -21,18 +64,12 @@ def axes_settings(ax: plt.Axes) -> None:
     ax.set_ylim(0, None)
 
 
-def add_histogram(ax: plt.Axes, from_catalog: str) -> None:
+def add_histogram(df: pd.DataFrame, ax: plt.Axes, from_catalog: str) -> None:
     # for key, val in SURVEY_COLOR_DICT.items():
-    in_file = (
-        config.RESULTS_CATALOGUE_DIR / "high-v_sources_old"
-        / "combined_vpec_med_gt_0_all.csv"
-    )
-
-    df = pd.read_csv(in_file)
     # sep_sigma_all = df["sep_x_g"] / df["pos_x_err"]
     # bins = np.linspace(sep_sigma_all.min(), sep_sigma_all.max(), 100)
     df_filtered = df[df["from"] == from_catalog]
-    sep = df_filtered["sep_x_g"] / df_filtered["pos_x_err"]
+    sep = df_filtered[ds.CombinedCatalogueSchema.sep_x_g_sigma].values
 
     kde = gaussian_kde(sep)
     sep_range = np.arange(0, max(sep), 0.01)
@@ -42,26 +79,28 @@ def add_histogram(ax: plt.Axes, from_catalog: str) -> None:
         color=SURVEY_COLOR_DICT_X[from_catalog],
         label=SURVEY_NAME_DICT_X[from_catalog]
     )
+    logger.info(
+        f"Added histogram for {from_catalog} with "
+        f"{df_filtered.shape[0]} sources."
+    )
     # _ = ax.hist(sep, bins=bins, density=True, histtype="step",
     # ec=SURVEY_COLOR_DICT[from_catalog],
     #             label=SURVEY_NAME_DICT[from_catalog], lw=1.5)
 
 
-def make_histogram() -> None:
+def make_histogram(df: pd.DataFrame) -> None:
     fig, ax = make_figure()
-
     for key in SURVEY_NAME_DICT_X.keys():
-        print(f"Done for {key}.")
-        add_histogram(ax, from_catalog=key)
+        add_histogram(df, ax, from_catalog=key)
 
     axes_settings(ax)
     plt.legend(loc="upper right")
-    plt.savefig(config.RESULTS_FIGURES_DIR / "sep_x_g_histogram.pdf")
-
-
-def main() -> None:
-    make_histogram()
 
 
 if __name__ == "__main__":
-    main()
+    df = _load_catalogue()
+    df = _add_from_colunmn(df)
+    make_histogram(df)
+    _save_to_figure(
+        config.RESULTS_FIGURES_DIR / "for_revision" / "sep_x_g_histogram.pdf"
+    )
