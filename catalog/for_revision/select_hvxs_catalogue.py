5777f1a17d92c476754d97f5b44b7ebede22dcc2
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-11-07 16:34:48 +0000

    Renamed

---
+++ catalog/for_revision/select_hvxs_catalogue.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import config
+import data_schema as ds
+import logging
+import numpy as np
+from log.log_config import configure_logging
+from catalog.for_revision.auto_correlate_xray_catalogues import (
+    _concatenate_catalogues
+)
+from pathlib import Path
+from typing import Literal
+
+
+
+configure_logging(level=logging.INFO, app_name=Path(__file__).stem)
+logger = logging.getLogger(Path(__file__).stem)
+
+
+def _filter_by_vpec_min_lim(
+        df: pd.DataFrame, vpec_min_lim: float,
+        vpec_min_lim_opt: Literal['lo', 'med']
+) -> pd.DataFrame:
+    df_copy = df.copy()
+
+    if vpec_min_lim_opt == 'lo':
+        vpec_col = (
+            df_copy[ds.CombinedCatalogueSchema.vpec_min_med]
+            - df_copy[ds.CombinedCatalogueSchema.e_vpec_min]
+        )
+    elif vpec_min_lim_opt == 'med':
+        vpec_col = df_copy[ds.CombinedCatalogueSchema.vpec_min_med]
+
+    else:
+        raise ValueError(f"Unknown vpec_min_lim_opt: {vpec_min_lim_opt}")
+
+    initial_len = df_copy.shape[0]
+
+    df_filtered = df_copy[
+        vpec_col >= vpec_min_lim
+    ].reset_index(drop=True)
+
+    final_len = df_filtered.shape[0]
+
+    logger.info(
+        f"Filtered by Vpec_min limit ({vpec_min_lim_opt}): "
+        f"from {initial_len} to {final_len}."
+    )
+
+    return df_filtered
+
+
+def _filter_by_fx_fg(df: pd.DataFrame) -> pd.DataFrame:
+    df_copy = df.copy()
+    logger.info(f"{df_copy.shape[0]} rows before FX/FG filtering.")
+    fx_fg = df_copy[ds.CombinedCatalogueSchema.fx_fg]
+    fx_fg_err = df_copy[ds.CombinedCatalogueSchema.fx_fg_err]
+    bp_rp = df_copy[ds.CombinedCatalogueSchema.bp_rp]
+
+    logger.info("Filtering by FX/FG ratio lower limit...")
+
+    _filter = (
+        fx_fg.notna() & fx_fg_err.notna() & bp_rp.notna()
+        & (fx_fg - fx_fg_err >= np.power(10, bp_rp - 3.5))
+    )
+
+    n_rows_removed = df_copy.shape[0] - _filter.sum()
+    df_filtered = df_copy[_filter].reset_index(drop=True)
+
+    logger.info(
+        f"{n_rows_removed} rows removed after FX/FG filtering, "
+        f"keeping {df_filtered.shape[0]} rows."
+    )
+
+    return df_filtered
+
+
+def _save_to_file(df: pd.DataFrame, file_path: Path) -> None:
+    df.to_csv(file_path, index=False)
+    logger.info(f"Saved DataFrame to {file_path}")
+
+
+def make_catalogue(
+        vpec_min_lim: float, vpec_min_lim_opt: Literal['lo', 'med']
+) -> pd.DataFrame:
+    df_master = _concatenate_catalogues()
+    logger.info(f"Concatenated catalogue shape: {df_master.shape}")
+
+
+if __name__ == "__main__":
+    make_catalogue(vpec_min_lim=200, vpec_min_lim_opt='lo')
