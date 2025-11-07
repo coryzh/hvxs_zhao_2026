f77e9a60180d0de5dba5b4c8e76039d874d6a9c0
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-11-07 18:41:04 +0000

    Add fx_fg_err attribute to CombinedCatalogueSchema

--- data_schema.py
+++ data_schema.py
@@ -345,6 +345,7 @@ class CombinedCatalogueSchema(Schema):
     lum_x: str = "lum_x"
     lum_x_err: str = "lum_x_err"
     fx_fg: str = "fx_fg"
+    fx_fg_err: str = "fx_fg_err"
     f_g: str = "f_g"
     from_: str = "from"
 
