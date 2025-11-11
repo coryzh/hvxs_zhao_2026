d63e919cbabf541e2d13d8a07141da12db8fbb1b
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2025-11-11 15:51:34 +0000

    Add remove_poorly_localised_xray_sources.py for cleaning poorly localized X-ray sources

---
+++ catalog/for_revision/remove_poorly_localised_xray_sources.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import logging
+import data_schema as ds
+from log.log_config import configure_logging
+from pathlib import Path
+
+
+configure_logging(level=logging.INFO, app_name=Path(__file__).stem)
+logger = logging.getLogger(Path(__file__).stem)
+
+
+def _clean(
+        df: pd.DataFrame, pos_x_err_thresh: float = 10.0, out_file: Path = None
+) -> pd.DataFrame:
+    logger.info(f"Catalogue loaded: {df.shape[0]} sources.")
+
+    _filter = df[ds.CombinedCatalogueSchema.pos_x_err] < pos_x_err_thresh
+
+    df = df[_filter].reset_index(drop=True)
+    n_removed = (~_filter).sum()
+    logger.info(
+        f"Catalogue cleaned: removed {n_removed} sources with "
+        f"pos_x_err < {pos_x_err_thresh} arcsec."
+        f" Remaining sources: {df.shape[0]}."
+    )
+
+    if out_file:
+        df.to_csv(out_file, index=False)
+
+    return df
