28952477eb435c5a00bd0772152b700e787ab891
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-01-07 19:32:11 +0000

    Updated the in and out names. Not a major update to be concern with since all recomputation was done on the server.

--- computation/calc_v_min_parallel.sh
+++ computation/calc_v_min_parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FILE_IN="../../results/combined/catalogues/for_revision/gaia/gaia_astrometry_60000_to_end.csv"
-OUT_FILE="../../results/combined/catalogues/for_revision/gaia/space_velocities_60000_to_end.csv"
+FILE_IN="../../results/combined/catalogues/for_revision/gaia/sources_to_be_recomputed.csv"
+OUT_FILE="../../results/combined/catalogues/for_revision/velocity/space_velocities_sources_to_be_recomputed.csv"
 
 python3 calc_v_min_parallel.py --in_cat_path "$FILE_IN" --out_file "$OUT_FILE" --method "scipy" --survey_name "concat" --batch_size 200 \
     --n_workers 7 --verbose True
