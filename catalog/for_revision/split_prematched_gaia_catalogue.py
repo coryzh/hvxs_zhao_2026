17d51f27a5868af89a1cdb5d326227a71a0aab2c
Yue (Cory) Zhao <32777663+coryzh@users.noreply.github.com>
2026-08-14 17:24:32 +0100

    Updated the path to the Gaia neighbour catalogue.

--- catalog/for_revision/split_prematched_gaia_catalogue.py
+++ catalog/for_revision/split_prematched_gaia_catalogue.py
@@ -65,7 +65,7 @@ def split_catalogue(df: pd.DataFrame) -> Dict[str, pd.DataFrame]:
 if __name__ == "__main__":
     in_file = (
         config.RESULTS_CATALOGUES_FOR_REVISION / "x_ray_catalogue_for_nway"
-        / "gaia_neighbours_20arcsec.fits"
+        / "gaia_neighbours_10arcsec.fits"
     )
 
     df = load_catalogue(in_file)
